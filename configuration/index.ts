cc228b92581fa81eb7a5fecc78870e49e5d4c2ad
ishandhanani <ishandhanani@gmail.com>
2023-06-05 15:24:48 -0500

    fix: token selection flow clearer for user to know which is base and quote

--- configuration/index.ts
+++ configuration/index.ts
@@ -37,10 +37,9 @@ async function botTypeUserCallback(): Promise<BotType> {
               console.log('Invalid answer! Pick a number 1 through 4');
               await askQuestion();
               break;
-            }
-        });
-    };
-    await askQuestion();
+            }});
+        };
+        await askQuestion();
     });
 }
 
@@ -123,7 +122,8 @@ async function tokenSelectionCallback(network: Network): Promise<TokenInfo[]> {
     const availableTokens = tokenList.tokens.filter(token => token.chainId === network);
     const availableTokensSymbols = availableTokens.map(token => token.symbol);
     return new Promise(resolve => {
-        console.log("These are the available tokens on your selected Network: ", availableTokensSymbols);
+        console.log("\nThese are the available tokens on your selected Network. Please choose a pair of tokens: ", availableTokensSymbols);
+        console.log("\nThe first asset you select will be your BASE ASSET and the second will be your QUOTE ASSET (ex. BASE/QUOTE)")
         rl.question('\n What tokens would you like to target in your strategy? (Enter the symbol of the token you want to target then enter to add, or enter "done" to finish):', (answer) => {
             if (answer.toLowerCase() === 'done') {
                 if (selectedTokens.length < 2){
@@ -140,6 +140,11 @@ async function tokenSelectionCallback(network: Network): Promise<TokenInfo[]> {
                     selectedTokens.push(selectedToken);
                 }
                 console.log('\n Selected tokens: ', selectedTokens);
+                if (selectedTokens.length === 1) {
+                    console.log(`\n${selectedTokens[0].symbol} selected as the base asset.\n`);
+                } else if (selectedTokens.length === 2) {
+                    console.log(`\n${selectedTokens[1].symbol} selected as the quote asset.\n`);
+                }
                 resolve(tokenSelectionCallback(network));
             } else {
                 console.log('Invalid answer! Pick a token symbol from the list or enter "done" to finish');
@@ -188,6 +193,7 @@ async function main() {
                         // 4. What tokens would you like to target in your strategy? 
                         return tokenSelectionCallback(selectedNetwork).then((selectedTokens: TokenInfo[]) => {
                             console.log("The user selected these tokens", selectedTokens);
+                            console.log(`\nThe user has selected to trade the following pair: ${selectedTokens[0].symbol}/${selectedTokens[1].symbol}\n`);
                             const botConfiguration: BotConfiguration = {
                                 botType: BotType.MarketMaking,
                                 strategy: selectedStrat,
