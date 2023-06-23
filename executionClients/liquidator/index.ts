e2f864d8201b1bf207430938af1697fabb177ee3
jossduff <jduff360@gmail.com>
2023-06-23 12:45:30 -0400

    Binary search for contract creation block

--- executionClients/liquidator/index.ts
+++ executionClients/liquidator/index.ts
@@ -4,12 +4,8 @@ import COMPTROLLER_INTERFACE from "../../configuration/abis/Comptroller";
 
 export async function startLiquidatorBot(configuration: BotConfiguration) {
     console.log("Starting Liquidator Bot.")
-
-    // get all accounts on startup [TODO] might have to use graph
-
-    // start listeners for all marketEntered and marketExited events
-
     
+    // TODO: move setup elsewhere to streamline
     var myProvider: ethers.providers.JsonRpcProvider | ethers.providers.WebSocketProvider;
     // pass through from config
     // Note that either a websocket or json rpc provider is allowed
@@ -24,33 +20,22 @@ export async function startLiquidatorBot(configuration: BotConfiguration) {
     // first test a call to closeFactorMantissa() view returns uint
     //console.log(await comptrollerInstance.closeFactorMantissa());
 
-    // const filter = {
-    //     address: "0x05Df6C772A563FfB37fD3E04C1A279Fb30228621",
-    //     topics: [
-    //         utils.id("MarketEntered(CToken,address)")
-    //     ]
-    // }
-    //let eventFilter = comptrollerInstance.filters.MarketEntered();
-
-    // TODO: make this work if query returns more than 10 000 events
     const currBlock = await myProvider.getBlockNumber();
     const fromBlock = currBlock - 100000;
     const toBlock = 'latest';
 
+    //console.log(currBlock);
+
+    const creationBlock = await findContractCreationBlock(myProvider, '0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B', 0, currBlock);
+
     const enterFilter = comptrollerInstance.filters['MarketEntered']()
     const exitFilter = comptrollerInstance.filters['MarketExited']()
-
+    
+    // TODO: make this work if query returns more than 10 000 events
     let enterEvents = await comptrollerInstance.queryFilter(enterFilter, fromBlock, toBlock);
     let exitEvents = await comptrollerInstance.queryFilter(exitFilter, fromBlock, toBlock);
 
-    // const enteredMarket = [];
-
-    // let account, cToken;
-    // for (const event of enterEvents) {
-    //     account = event.args.account
-    //     cToken = event.args.cToken;
-    //     enteredMarket.push({ account, cToken });
-    // }
+    // build array of accounts who have entered a market
     const enteredMarkets = enterEvents.map(event => ({
         account: event.args.account,
         cToken: event.args.cToken,
@@ -58,15 +43,7 @@ export async function startLiquidatorBot(configuration: BotConfiguration) {
     }));
     //console.log(enteredMarkets);
 
-
-    //console.log("\nEXITED markets\n");
-    // const exitedMarket = [];
-
-    // for (const event of enterEvents) {
-    //     account = event.args.account
-    //     cToken = event.args.cToken;
-    //     exitedMarket.push({ account, cToken });
-    // }
+    // build array of accounts who have exited a market
     const exitedMarkets = exitEvents.map(event => ({
         account: event.args.account,
         cToken: event.args.cToken,
@@ -74,9 +51,11 @@ export async function startLiquidatorBot(configuration: BotConfiguration) {
     }));
     //console.log(exitedMarkets);
 
-    // iterates over every element in enteredMarkets and only keeps the elements if it does not also exist in exitedMarkets
+    // compare entered and exited arrays.  Result: filteredEnteredMarkets entries are only currently open positions
+    // iterates over every element in enteredMarkets and removes the element if it matches an entry in exitedMarkets
     const filteredEnteredMarkets = enteredMarkets.filter(currEnteredMarketAccount => {
 
+        // Searches exitedMarkets for entries that match (on account address and cToken address) current account from enteredMarkets
         const matched = exitedMarkets.find(currExitedMarketAccount =>
             currExitedMarketAccount.account == currEnteredMarketAccount.account &&
             currExitedMarketAccount.cToken == currEnteredMarketAccount.cToken && 
@@ -84,19 +63,36 @@ export async function startLiquidatorBot(configuration: BotConfiguration) {
         );
 
         if (matched) {
-            console.log('Matching entry found: ', currEnteredMarketAccount);
+            //console.log('Matching entry found: ', currEnteredMarketAccount);
             return false; // remove this entry from enteredMarkets
         }
         else {
             return true; // keep this entry in enteredMarkets
         }
-
-    //     !exitedMarkets.some(exitedMarket =>
-    //         exitedMarket.account == currEnteredMarketAccount.account &&
-    //         exitedMarket.cToken == currEnteredMarketAccount.cToken
-    //     )
     });
-
-    
 }
 
+// Binary search on-chain to find block at which contract is created
+// Use returned block number as starting block to find historical positions (positions opened before bot creation)
+// TODO: should I not pass in provider?
+// TODO: this is ugly as hell.  Refactor to trim parameters.  Shouldn't have to pass provider each time
+async function findContractCreationBlock(myProvider: ethers.providers.JsonRpcProvider | ethers.providers.WebSocketProvider, address: string, startBlock: number, endBlock: number): Promise<number> {
+
+    if (startBlock >= endBlock) {
+        console.log("Found creation block: " + startBlock);
+        return startBlock;
+    }
+
+    let midBlock = Math.floor((startBlock + endBlock) / 2);
+    // returns contract code (as hex) of address at midBlock.  If no contract is deployed, returns '0x'
+    let codeHex = await myProvider.getCode(address, midBlock);
+
+    if (codeHex == "0x") {
+        // contract hasn't been created yet, search upper half
+        return findContractCreationBlock(myProvider, address, midBlock+1, endBlock);
+    }
+    else {
+        // contract was already created, search lower half
+        return findContractCreationBlock(myProvider, address, startBlock, midBlock-1);
+    }
+}
